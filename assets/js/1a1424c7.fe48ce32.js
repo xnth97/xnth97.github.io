7d0774e6297e690345e0b167b30b857ed5048c4d
xnth97 <xnth97@live.com>
2025-01-19 07:13:56 +0000

    Update from https://github.com/xnth97/blog/commit/1ba83258fdaf953b6b04abb630657068c23d4181

---
+++ assets/js/1a1424c7.fe48ce32.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([["9763"],{70829:function(e){e.exports=JSON.parse('{"metadata":{"permalink":"/","page":1,"postsPerPage":10,"totalPages":3,"totalCount":24,"nextPage":"/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
